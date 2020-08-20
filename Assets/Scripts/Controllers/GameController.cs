30b3162d0f903f16d8ab96fcc1d8b7bc0e63a295
Paulo H <paulo@ninito.me>
2020-08-20 03:36:11 -0300

    Code UIController, GameDifficultySO, start GameController
    Removed "skills" folder from project, updated pitch document, implement GameDifficultySO, finish UIController, started GameController. Also added MonKey plugin to project.

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    #region Field Declarations
+
+    #region  Core Values
+
+    private int wave = 1;
     private float score = 0;
+    private GameDifficultySO difficultyConfig;
+    private Queue<EnemyBase> enemiesToSpawn;
+    private Queue<PowerupBase> powerupsToSpawn;
+    private List<EnemyBase> activeEnemies;
+    private List<GameObject> spawnPoints;
+    private List<GameObject> spawnablesList;
+    private Coroutine spawnEnemiesRoutine;
+    private Coroutine spawnPowerupsRoutine;
+    private PlayerController playerReference;
+
+    #endregion
+
+    #region Properties
+
+    public int Wave
+    {
+        get { return wave; }
+    }
 
     public float Score
     {
         get { return score; }
     }
 
+    public PlayerController PlayerReference
+    {
+        get { return playerReference; }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Unity Methods
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Gets a reference of playerController so other classes can access it
+        playerReference = FindObjectOfType<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,4 +56,111 @@ public class GameController : MonoBehaviour
     {
         
     }
+    #endregion
+
+    #region Custom Methods
+
+    private void StartGame()
+    {
+        
+    }
+
+    private void EndGame()
+    {
+
+    }
+
+    private void BeginWave()
+    {
+
+    }
+
+    private void EndWave()
+    {
+
+    }
+
+    private GameObject PickEnemy()
+    {
+        // Uses the system randomizer, which is considerably better than Unity's
+        System.Random trueRandomizer = new System.Random();
+        int randomIndexPick = trueRandomizer.Next(1, 100);
+
+        // Randomly draw enemy type from spawnable enemy pool
+        GameObject enemyToSpawn = spawnablesList[randomIndexPick];
+        return enemyToSpawn;
+    }
+
+    // Populates the list containing possibilities for spawning an enemy
+    private void PopulateSpawnablesList(bool repopulate = false)
+    {
+        // If the list doesn't exist, create it.
+        if (spawnablesList == null)
+        {
+            spawnablesList = new List<GameObject>();
+        }
+
+        // Checks to see if the list is meant to be REpopulated, meaning it needs to be cleared and populated again
+        if (repopulate == true)
+        {
+            spawnablesList.Clear();
+        }
+
+        //FIXME: This code is MONSTROUSLY better than our first implementation, but it still remains relatively unextensible
+        // Populates list with roller tank entries
+        for (int spawnEntry = 0; spawnEntry < difficultyConfig.RollerTankChance; spawnEntry ++)
+        {
+            spawnablesList.Add(difficultyConfig.RollerTank);
+        }
+
+        // Populates list with speeder tank entries
+        for (int spawnEntry = 0; spawnEntry < difficultyConfig.SpeederTankChance; spawnEntry++)
+        {
+            spawnablesList.Add(difficultyConfig.SpeederTank);
+        }
+
+        // Populates list with smasher tank entries
+        for (int spawnEntry = 0; spawnEntry < difficultyConfig.SmasherTankChance; spawnEntry++)
+        {
+            spawnablesList.Add(difficultyConfig.SmasherTank);
+        }
+
+        // Populates list with annihilator tank entries
+        for (int spawnEntry = 0; spawnEntry < difficultyConfig.AnnihilatorTankChance; spawnEntry++)
+        {
+            spawnablesList.Add(difficultyConfig.AnnihilatorTank);
+        }
+
+        // Checks if list has more or less than 100 entries.
+        if (spawnablesList.Count > 100)
+        {
+            int listOverpopulationCount = spawnablesList.Count - 100;
+            Debug.LogWarningFormat("Overpopulated list by {0} entries. Full entry count at {1}", listOverpopulationCount, spawnablesList.Count);
+        }
+        else if (spawnablesList.Count < 100)
+        {
+            int listUnderpopulationCount = 100 - spawnablesList.Count;
+            Debug.LogWarningFormat("Underpopulated list by {0} entries. Full entry count at {1}", listUnderpopulationCount, spawnablesList.Count);
+        }
+    }
+
+    #region Coroutines
+
+    // Coroutine for periodically spawning enemies
+    private IEnumerator SpawnEnemy()
+    {
+
+        // Spawn enemy on spawn points
+        yield return null;
+    }
+
+    // Coroutine for periodically spawning powerups
+    private IEnumerator SpawnPowerup()
+    {
+        yield return null;
+    }
+
+    #endregion
+
+    #endregion
 }
--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -13,13 +13,23 @@ public class UIController : MonoBehaviour
     private int displayedScore;
     private Text ammoCountText;
     private Text scoreText;
+    [Header("Icons")]
+    [SerializeField]
+    [Tooltip("Fire cooldown icon sprite")]
     private Image fireCooldownIcon;
+    [SerializeField]
+    [Tooltip("Heart fill icon sprite")]
+    private Image heartIcons;
+    [Header("Linear interpolation speeds")]
+    [SerializeField]
+    [Tooltip("The linear interpolation speed at which the score number is updated")]
     private float scoreLerpSpeed;
-    private float healthLerpSpeed;
-
-    // Images containing the backdrop for counters
-    private Image healthBackdrop;
-    private Image ammoBackdrop;
+    [Tooltip("The linear interpolation speed at which the health fill is updated")]
+    [SerializeField]
+    private float healthLerpSpeed = 13f;
+    [Tooltip("The linear interpolation speed at which the fire readiness icon is updated")]
+    [SerializeField]
+    private float fireCooldownLerpSpeed = 13f;
 
     // Useful object references
     private GameController gameController;
@@ -31,16 +41,38 @@ public class UIController : MonoBehaviour
 
     #region Unity Methods
 
+    // Awake runs before the first frame
     private void Awake()
     {
+        // Subscribes ammo update to shot fired by player event
         EventBroker.ShotFired += UpdateAmmo;
+    }
+
+    private void Start() 
+    {
+        gameController = FindObjectOfType<GameController>();
+        playerController = gameController.PlayerReference;
+    }
+
+    // Update runs every frame
+    private void Update()
+    {
+        UpdateScore();
+        UpdateFireCooldown();
+        
+        // Checks if a change in health has occurred to prevent the expensive lerp operation from running all the time
+        // Compares a float to a int using Approximately to prevent floating point comparison imprecision error
+        if (Mathf.Approximately(heartIcons.fillAmount, playerController.Health))
+        {
+            UpdateHealth();
+        }
     }
 
     #endregion
 
     #region Custom Methods
 
-    // Updates score number
+    // Updates score number with a crisp lerped effect
     private void UpdateScore()
     {
         // The score number is linearly interpolated for a crisp score increase effect
@@ -50,13 +82,14 @@ public class UIController : MonoBehaviour
         scoreText.text = displayedScore.ToString();
     }
 
-    //TODO: Implement new health system and UpdateHealth method
+    // Increases and decreases heart icon fill with a cris
     private void UpdateHealth()
     {
-
+        // Time.deltaTime is multiplied by 13 - a number reached through testing.
+        heartIcons.fillAmount = Mathf.Lerp(heartIcons.fillAmount, playerController.Health / playerController.MaxHealth, Time.deltaTime * healthLerpSpeed);
     }
 
-    //TODO: Implement player fire override method so you can trigger this method through an action
+    // Updates ammo count text
     private void UpdateAmmo()
     {
         ammoCountText.text = playerController.Ammo.ToString();
@@ -66,9 +99,8 @@ public class UIController : MonoBehaviour
     private void UpdateFireCooldown()
     {
         // Time.deltaTime is multiplied by 13 - a number reached through testing.
-        fireCooldownIcon.fillAmount = Mathf.Lerp(fireCooldownIcon.fillAmount, playerController.FireCooldown / playerController.MaxFireCooldown, Time.deltaTime * 13);
+        fireCooldownIcon.fillAmount = Mathf.Lerp(fireCooldownIcon.fillAmount, playerController.FireCooldown / playerController.MaxFireCooldown, Time.deltaTime * fireCooldownLerpSpeed);
     }
-
 
     #endregion
 }
