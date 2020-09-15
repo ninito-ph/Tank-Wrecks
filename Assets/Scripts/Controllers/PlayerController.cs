2650b6623cb61787d112bb737db1bbc40435bb77
Paulo H <paulo@ninito.me>
2020-09-15 02:18:34 -0300

    Remove Debug command
    Removed a debug command from the player script.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -122,7 +122,6 @@ public class PlayerController : TankBase
                 {
                     CreateProjectile(currentCannon, currentCannon, nukeShell);
                     nukeShellAmmo--;
-                    Debug.Log("Fired nuke shell");
                 }
                 else
                 {
