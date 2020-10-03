60648abf19f35970c32193c4528dbe1a1980e28d
Paulo H <paulo@ninito.me>
2020-10-03 06:16:19 -0300

    Add LeaderboardController and OptionsMenuController scripts
    Added LeaderboardController and OptionsMenuController empty script files for future development.

--- Assets/Scripts/Controllers/UI/OptionsMenuController.cs
+++ Assets/Scripts/Controllers/UI/OptionsMenuController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuController : MonoBehaviour
+public class OptionsMenuController : MenuBase
 {
     // Start is called before the first frame update
     void Start()
