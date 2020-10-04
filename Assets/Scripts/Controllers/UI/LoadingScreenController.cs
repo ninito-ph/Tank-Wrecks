424897911c675ae8aaea3f971db0819469dbc0ac
Paulo H <paulo@ninito.me>
2020-10-04 13:03:15 -0300

    Add Loading Screen
    Added Loading Screen and underlying functionality scripts. Also updated dependent code and created prefabs for the loading screen.

--- Assets/Scripts/Controllers/UI/LoadingScreenController.cs
+++ Assets/Scripts/Controllers/UI/LoadingScreenController.cs
@@ -15,19 +15,19 @@ public class LoadingScreenController : MonoBehaviour
     [Header("Interface")]
     [SerializeField]
     [Tooltip("The loading bar of the loading screen")]
-    private Image loadingBar;
+    private Image loadingBarFill;
     [SerializeField]
     [Tooltip("The text element containing space for the tips")]
     private Text tipsText;
+    [SerializeField]
+    [Tooltip("The image element through which the displayed object is displayed")]
+    private RawImage displayObjectImage;
 
     // Visual content to distract the player while the scene loads
     [Header("Visual Content")]
     [SerializeField]
-    [Tooltip("The possible tips in the loading screen. Note the models and tips must be correlated.")]
-    private string[] tips;
-    [SerializeField]
-    [Tooltip("The possible models in the loading screen. Note the models and tips must be correlated.")]
-    private GameObject[] displayableModels;
+    [Tooltip("The possible objects to be displayed in the loading screen")]
+    private GameObject[] displayableObjects;
     [SerializeField]
     [Tooltip("The place at which to create the displayable prefab")]
     private GameObject displayOrigin;
@@ -35,8 +35,12 @@ public class LoadingScreenController : MonoBehaviour
     [Tooltip("The speed at which to spin the displayed object")]
     private Vector3 rotationSpeed = new Vector3(0f, 5f, 0f);
 
+    // Stores cache of mainCamera
+    private Camera mainCamera;
+
     // The model that was put on display for the duration loading screen
     private GameObject displayedObject;
+    private Texture2D displayedObjectTexture;
 
     #endregion
 
@@ -44,16 +48,32 @@ public class LoadingScreenController : MonoBehaviour
 
     private void Start()
     {
-        // Starts the loading process and unloads previous scene
-        loadScene = SceneManager.LoadSceneAsync(LoadData.SceneToLoad, LoadSceneMode.Single);
+
+        // TODO: Remove this null check once testing is done
+        if (LoadData.SceneToLoad != null)
+        {
+            // Starts the loading process and unloads previous scene
+            loadScene = SceneManager.LoadSceneAsync(LoadData.SceneToLoad, LoadSceneMode.Single);
+        }
+        else
+        {
+            // Returns to the menu as a failsafe
+            loadScene = SceneManager.LoadSceneAsync(0);
+        }
 
         // Randomly picks an object and its correlated tip
-        int randomPick = Random.Range(0, Mathf.Min(tips.Length, displayableModels.Length));
+        int randomPick = Random.Range(0, displayableObjects.Length);
 
-        // Sets the tip to that of the correlated object
-        tipsText.text = tips[randomPick];
         // Instantiates the displayable model
-        displayedObject = Instantiate(displayableModels[randomPick], displayedObject.transform.position, Quaternion.identity);
+        displayedObject = Instantiate(displayableObjects[randomPick], displayOrigin.transform.position, Quaternion.identity);
+        // Enables the display mode on an object, which returns a tip
+        tipsText.text = displayedObject.GetComponent<DisplayableObjectController>().LoadingTip;
+
+        // Caches main camera
+        mainCamera = Camera.main;
+
+        // Sets up display texture
+        SetupRenderTexture(512);
 
         // Clears LoadData SceneToLoad
         LoadData.SceneToLoad = null;
@@ -61,13 +81,51 @@ public class LoadingScreenController : MonoBehaviour
 
     private void Update()
     {
+        // Updates the fill amount of the loading bar
+        // progess is being divided by a total of 0.9 instead of 1; at 1 the scene has finished loading, and the scene changes automatically
+        // TODO: Remove this null check once testing is done
+        if (loadScene != null)
+        {
+            loadingBarFill.fillAmount = Mathf.Clamp01(loadScene.progress / 0.9f);
+        }
+
         // Rotates the displayed object
         displayedObject.transform.Rotate(rotationSpeed * Time.deltaTime, Space.World);
 
-        // Updates the fill amount of the loading bar
-        // progess is being divided by a total of 0.9 instead of 1; at 1 the scene has finished loading, and the scene changes automatically
-        loadingBar.fillAmount = Mathf.Clamp01(loadScene.progress / 0.9f);
+        // Updates the display texture
+        displayObjectImage.texture = DisplayObjectToTexture(displayedObjectTexture.height);
     }
+
+    #region Custom Methods
+
+    private void SetupRenderTexture(int textureSize)
+    {
+        // Initializes texture
+        displayedObjectTexture = new Texture2D(textureSize, textureSize);
+
+        // Sets the target render texture of the camera to Unity's temporary texture
+        // temporary textures usually aren't used this way, but doing this causes no issues
+        RenderTexture temporaryRenderTexture = RenderTexture.GetTemporary(textureSize, textureSize, 16, RenderTextureFormat.Default, RenderTextureReadWrite.Default, 2);
+        mainCamera.targetTexture = temporaryRenderTexture;
+    }
+
+    // Renders the display object to a texture
+    private Texture2D DisplayObjectToTexture(int textureSize)
+    {
+        // Renders the camera's view
+        mainCamera.Render();
+
+        // Sets the camera's target texture to the active render texture
+        RenderTexture.active = mainCamera.targetTexture;
+
+        // Saves render texture into texture
+        displayedObjectTexture.ReadPixels(new Rect(0f, 0f, displayedObjectTexture.width, displayedObjectTexture.height), 0, 0);
+        displayedObjectTexture.Apply();
+
+        return displayedObjectTexture;
+    }
+
+    #endregion
 
     #endregion
 }
--- Assets/Scripts/LoadData.cs
+++ Assets/Scripts/LoadData.cs
@@ -1,14 +1,16 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 // A class that carries data through scenes
 public static class LoadData
 {
     #region Field Declarations
     // The next scene to load
-    private static string sceneToLoad;
+    private static string sceneToLoad = null;
 
     #region Properties
-    
+
     public static string SceneToLoad
     {
         get { return sceneToLoad; }
