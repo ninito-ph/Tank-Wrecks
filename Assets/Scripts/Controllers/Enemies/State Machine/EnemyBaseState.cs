c4f964e1fc5fb10f85956607b254e786c79cd3f1
Paulo H <paulo@ninito.me>
2020-10-28 22:46:52 -0300

    Update base AI state
    Added LeaveState and OnDrawGizmos method

--- Assets/Scripts/Controllers/Enemies/State Machine/EnemyBaseState.cs
+++ Assets/Scripts/Controllers/Enemies/State Machine/EnemyBaseState.cs
@@ -2,9 +2,15 @@
 
 public abstract class EnemyBaseState
 {
-    // Start is called whenever the enemy enters a state
+    // EnterState is called whenever the enemy enters a state
     public abstract void EnterState(EnemyBase enemy);
+
+    // LeaveState is called whenever the enemy leaves a state
+    public abstract void LeaveState(EnemyBase enemy);
 
     // Update is called once per frame
     public abstract void Update(EnemyBase enemy);
+
+    // OnDrawGizmos is called once every time gizmos are drawn to the scene view
+    public abstract void OnDrawGizmos(EnemyBase enemy);
 }
