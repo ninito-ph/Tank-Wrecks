e4550cf1d516e1ea3c3d6fc741c4fc954da33e47
Paulo H <paulo@ninito.me>
2020-10-28 22:49:29 -0300

    Remove temporary AI implementation
    Removed temporary AI implementation from RollerController script

--- Assets/Scripts/Controllers/Enemies/RollerController.cs
+++ Assets/Scripts/Controllers/Enemies/RollerController.cs
@@ -5,26 +5,5 @@ using UnityEngine.AI;
 
 public class RollerController : EnemyBase
 {
-    private Coroutine gotoPlayerRoutine;
-    public NavMeshAgent agent;
-    public GameObject player;
 
-    protected override void Start()
-    {
-        base.Start();
-        player = GameObject.Find("Tank (Player)");
-        agent = gameObject.GetComponent<NavMeshAgent>();
-
-        Debug.Log("Pathfinding: " + agent + ", player: " + playerReference);
-
-        gotoPlayerRoutine = StartCoroutine(AIPathToPlayer());
-    }
-
-    private IEnumerator AIPathToPlayer()
-    {
-        TankFire();
-        Vector3 destination = player.GetComponent<PlayerController>().bodyPosition;
-        agent.SetDestination(destination);
-        yield return new WaitForSeconds(10f);
-    }
 }
