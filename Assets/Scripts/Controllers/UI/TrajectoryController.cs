8a55ba8c75ad15e909f4c118b226539b60d95dac
Paulo H <paulo@ninito.me>
2020-11-13 20:08:32 -0300

    Refactor TrajectoryController
    - Removed unnecessary using directives
    - Moved Projectile Launch Data into a different file
    - Implemented Backwards Euler projectile arc prediction
    - Decoupled TrajectoryController from TankBase

--- Assets/Scripts/Controllers/UI/TrajectoryController.cs
+++ Assets/Scripts/Controllers/UI/TrajectoryController.cs
@@ -1,23 +1,18 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TrajectoryController : MonoBehaviour
 {
     #region Field Declarations
 
-    [SerializeField]
-    [Tooltip("The amount of samples the trajectory preview line will display")]
-    private int sampleCount;
-
-    private Vector3 finalPosition = new Vector3(0, 0, 0);
-
+    // The speed at which the projectile is launched
+    private ProjectileLaunchData launchData;
     // The line renderer component cache
     private LineRenderer lineRenderer;
-    // The reference to the tank who fired the projectile
-    private TankBase tank;
     // The coroutine responsile for updating the trajectory
     private Coroutine updateRoutine;
+
+    public ProjectileLaunchData LaunchData { get => launchData; set => launchData = value; }
 
     #endregion
 
@@ -26,8 +21,6 @@ public class TrajectoryController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Gets the tank who fired the projectile
-        tank = transform.root.GetComponent<TankBase>();
         // Gets the line renderer's component cache
         lineRenderer = gameObject.GetComponent<LineRenderer>();
         // Starts the update routine
@@ -40,129 +33,55 @@ public class TrajectoryController : MonoBehaviour
     // Coroutine to update the trajectory
     private IEnumerator UpdateTrajectory()
     {
+        // Caches WaitFor class
+        WaitForFixedUpdate waitFixedFrame = new WaitForFixedUpdate();
+
         // Loops eternally
         while (true)
         {
-            // Retrieves the launch info from the tank
-            ProjectileLaunchData launchData = tank.LaunchData;
-
             // Updates the trajectory preview
-            UpdateTrajectoryPreview(launchData);
+            DrawTrajectoryPreview();
 
             // Waits until the next frame
-            yield return null;
+            yield return waitFixedFrame;
         }
     }
     #endregion
-
 
     #region Custom Methods
 
-    /// <summary>
-    /// Updates the line renderer with the trajectory's preview
-    /// </summary>
-    /// <param name="launchData">Information about the projectile's launch</param>
-    private void UpdateTrajectoryPreview(ProjectileLaunchData launchData)
+    private void DrawTrajectoryPreview()
     {
-        // Updates the line renderer's position count and passes it an array of
-        // vectors containing the sampled positions
-        lineRenderer.positionCount = sampleCount + 1;
-        lineRenderer.SetPositions(GetTrajectoryPreview(sampleCount, launchData));
-    }
+        // Gets the timestep
+        float timeStep = Time.fixedDeltaTime * 1f;
+        // Calculates the target splash
+        float targetSplash = LaunchData.GetProjectileSplash(timeStep);
 
-    /// <summary>
-    /// Gets the preview of a projectile's trajectory
-    /// </summary>
-    /// <param name="samples">The number of times to sample the trajectory</param>
-    /// <param name="launchData">The information about the projetile's launch</param>
-    /// <returns>A Vector3 array containing the points sampled</returns>
-    private Vector3[] GetTrajectoryPreview(int samples, ProjectileLaunchData launchData)
-    {
-        // The array containing the sampled points
-        Vector3[] previewPoints = new Vector3[samples + 1];
+        // Calculates the amount of necessary segments in the line
+        int positionCount = Mathf.RoundToInt(targetSplash / timeStep);
+        // Updates linerenderer's position amount
+        lineRenderer.positionCount = positionCount;
 
-        // Gets the interval in time in the trajectory between samples
-        float sampleTimeInverval = launchData.GetProjectileAirTime() / samples;
+        // Initializes necessary values to keep track of projectile path
+        Vector3 currentVelocity = LaunchData.launchVelocity;
+        Vector3 currentPosition = transform.position;
 
-        // Gathers samples progressively until it reaches the end of the projectile range
-        for (int sample = 0; sample < previewPoints.Length; sample++)
+        Vector3 newPosition = Vector3.zero;
+        Vector3 newVelocity = Vector3.zero;
+
+        // Calculates and draws the trajectory line
+        for (int positionIndex = 0; positionIndex < positionCount; positionIndex++)
         {
-            float sampleTime = sampleTimeInverval * sample;
-            previewPoints[sample] = GetTrajectoryPoint(launchData.launchVelocity, sampleTime, launchData.projectileGravity);
+            // Gives the linerenderer the current position
+            lineRenderer.SetPosition(positionIndex, currentPosition);
+            // Uses backwards euler method to increment position
+            LaunchData.BackwardsEuler(timeStep, currentPosition, currentVelocity, out newPosition, out newVelocity);
+
+            // Updates current position and velocity
+            currentPosition = newPosition;
+            currentVelocity = newVelocity;
         }
-
-        // Returns the sampled points aray
-        return previewPoints;
-    }
-
-    /// <summary>
-    /// Gets point in projectile trajectory accouring to time
-    /// </summary>
-    /// <param name="launchVelocity">The launch velocity of the projectile</param>
-    /// <param name="sampleTime">The time at which to take the sample of the trajectory</param>
-    /// <param name="projectileGravity">The gravity affecting the projectile</param>
-    /// <returns></returns>
-    private Vector3 GetTrajectoryPoint(Vector3 launchVelocity, float sampleTime, float projectileGravity)
-    {
-        // Gets the X, Y and Z positions of the sample
-        float positionX = (launchVelocity.x * sampleTime);
-        float positionY = (launchVelocity.y * sampleTime) - (projectileGravity * Mathf.Pow(sampleTime, 2) / 2);
-
-        // Returns positions in a Vector3
-        return new Vector3(0f, positionY, positionX);
     }
 
     #endregion
-}
-
-/// <summary>
-/// Contains the information about a projectile's launch necessary to calculate its trajectory
-/// </summary>
-public struct ProjectileLaunchData
-{
-    /// <summary>
-    /// The velocity at which the projectile is launched
-    /// </summary>
-    public Vector3 launchVelocity;
-    /// <summary>
-    /// The gravity affecting the projectile during its flight
-    /// </summary>
-    public float projectileGravity;
-    /// <summary>
-    /// The launch angle of the projectile
-    /// </summary>
-    public float launchAngle;
-    /// <summary>
-    /// How far from the ground the projectile was launched from
-    /// </summary>
-    public float launchHeight;
-
-    /// <summary>
-    /// Constructs the projectile launch data
-    /// </summary>
-    /// <param name="launchVelocity">The velocity at which the projectile is launched</param>
-    /// <param name="projectileGravity">The gravity affecting the projectile during its flight</param>
-    /// <param name="launchAngle">The launch angle of the projectile</param>
-    /// <param name="launchHeight">How far from the ground the projectile was launched from</param>
-    public ProjectileLaunchData(Vector3 launchVelocity, float projectileGravity, float launchAngle, float launchHeight)
-    {
-        // Initializes the struct's variables
-        this.launchVelocity = launchVelocity;
-        this.projectileGravity = projectileGravity;
-        this.launchAngle = launchAngle;
-        this.launchHeight = launchHeight;
-    }
-
-    /// <summary>
-    /// Calculates how long for the projectile will remain on the air
-    /// </summary>
-    /// <returns>How long the projectile will remain on the air for</returns>
-    public float GetProjectileAirTime()
-    {
-        // Calculates the quadractic equation necessary to define the airtime
-        float airTime = (launchVelocity.y + Mathf.Sqrt((launchVelocity.y * launchVelocity.y) + 2 * projectileGravity * launchHeight)) / projectileGravity;
-
-        // Returns the airtime
-        return airTime;
-    }
 }
---
+++ Assets/Scripts/Other/ProjectileLaunchData.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Contains the information about a projectile's launch necessary to calculate its trajectory
+/// </summary>
+public struct ProjectileLaunchData
+{
+    /// <summary>
+    /// The velocity at which the projectile is launched
+    /// </summary>
+    public Vector3 launchVelocity;
+    /// <summary>
+    /// The gravity affecting the projectile during its flight
+    /// </summary>
+    public float projectileGravity;
+    /// <summary>
+    /// The launch angle of the projectile
+    /// </summary>
+    public float launchAngle;
+    /// <summary>
+    /// How far from the ground the projectile was launched from
+    /// </summary>
+    public float launchHeight;
+
+    /// <summary>
+    /// Constructs the projectile launch data
+    /// </summary>
+    /// <param name="launchVelocity">The velocity at which the projectile is launched</param>
+    /// <param name="projectileGravity">The gravity affecting the projectile during its flight</param>
+    /// <param name="launchAngle">The launch angle of the projectile</param>
+    /// <param name="launchHeight">How far from the ground the projectile was launched from</param>
+    public ProjectileLaunchData(Vector3 launchVelocity, float projectileGravity, float launchAngle, float launchHeight)
+    {
+        // Initializes the struct's variables
+        this.launchVelocity = launchVelocity;
+        this.projectileGravity = projectileGravity;
+        this.launchAngle = launchAngle;
+        this.launchHeight = launchHeight;
+    }
+
+    /// <summary>
+    /// Gets the "splash" of the projectile. Splash is the artillery term for projectile airtime.
+    /// </summary>
+    /// <param name="timeStep">The lenght of the timestep for the physics calculation. Numbers too large will be innacurate, as will numbers too small due to rounding innacuracies.</param>
+    /// <returns></returns>
+    public float GetProjectileSplash(float timeStep)
+    {
+        // Stores the current velocity and position
+        Vector3 currentVelocity = launchVelocity;
+        Vector3 currentPosition = new Vector3(0f, launchHeight, 0f);
+
+        // Stores the new position and velocity
+        Vector3 newVelocity = Vector3.zero;
+        Vector3 newPosition = Vector3.zero;
+
+        // The time it will take to reach target (splash)
+        float splash = 0f;
+
+        // Limit time to prevent infinite loops
+        for (splash = 0f; splash < 30f; splash += timeStep)
+        {
+            // The bullet's position is calculted using numerical integration,
+            // more specifically, backwards euler numerical integration. This
+            // method is not identical, but almost undistinguishable from what
+            // Unity uses. This yields the best results for rigidbody projectile
+            // motion prediction.
+            BackwardsEuler(timeStep, currentPosition, currentVelocity, out newPosition, out newVelocity);
+
+            // If we are moving downwards and are below ground level, we have
+            // obligatorily hit something
+            if (newPosition.y < currentPosition.y && newPosition.y < 0f)
+            {
+                // Adds twice the timestep to to make sure ground is reached
+                splash += timeStep * 2f;
+                // Ends loop prematurely to save resources
+                break;
+            }
+
+            // Saves predicted position and velocity
+            currentPosition = newPosition;
+            currentVelocity = newVelocity;
+        }
+
+        // Returns the projectile's splash
+        return splash;
+    }
+
+    /// <summary>
+    /// Numerical integration method to simulate projectile motion. Backwards Euler is very similar to what is used in Unity, and thus yields the best results in this situation
+    /// </summary>
+    /// <param name="timeStep">The lenght of the timestep for the physics calculation. Numbers too large will be innacurate, as will numbers too small due to rounding innacuracies.</param>
+    /// <param name="currentPosition">The current position of the projectile.</param>
+    /// <param name="currentVelocity">The current velocity of the projectile.</param>
+    /// <param name="newPosition">The output position of the projectile.</param>
+    /// <param name="newVelocity">The output velocity of the projectile.</param>
+    public void BackwardsEuler(float timeStep, Vector3 currentPosition, Vector3 currentVelocity, out Vector3 newPosition, out Vector3 newVelocity)
+    {
+        // Gets accelerations acting on the projectile
+        Vector3 accelerationForces = Physics.gravity;
+
+        // Calculates the new position and acceleration accourding to timestep
+        newVelocity = currentVelocity + timeStep * accelerationForces;
+        newPosition = currentPosition + timeStep * newVelocity;
+    }
+}
